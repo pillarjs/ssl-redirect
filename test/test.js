ed24f27b7d1519f87bbe60482466e73a922f0093
Jonathan Ong <jonathanrichardong@gmail.com>
2014-09-08 22:19:29 -0700

    add more tests

--- test/test.js
+++ test/test.js
@@ -4,20 +4,56 @@ var request = require('supertest')
 var server = require('..')()
 
 describe('SSL Redirect', function () {
-  describe('GET', function () {
-    it('should return 301 w/ Location, STS, and CC headers', function (done) {
+  describe('HEAD', function () {
+    it('should return 301 w/ Location, STS', function (done) {
       request(server)
-      .get('/')
+      .head('/')
       .expect('Location', /https/)
       .expect('Strict-Transport-Security', /max-age/)
       .expect(301, done)
     })
   })
 
+  describe('GET', function () {
+    it('should return 301 w/ Location, STS', function (done) {
+      request(server)
+      .get('/')
+      .expect('Location', /https/)
+      .expect('Strict-Transport-Security', /max-age/)
+      .expect(301, done)
+    })
+
+    it('should work with accept: text/plain', function (done) {
+      request(server)
+      .get('/')
+      .set('Accept', 'text/plain')
+      .expect('Content-Type', /text\/plain/)
+      .expect(301, done)
+    })
+  })
+
   describe('POST', function () {
-    it('should return 403 w/ STS and CC headers', function (done) {
+    it('should return 403 w/ STS', function (done) {
       request(server)
       .post('/')
+      .expect('Strict-Transport-Security', /max-age/)
+      .expect(403, done)
+    })
+
+    it('w/ accept: json', function (done) {
+      request(server)
+      .post('/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /application\/json/)
+      .expect('Strict-Transport-Security', /max-age/)
+      .expect(403, done)
+    })
+
+    it('w/ accept: text', function (done) {
+      request(server)
+      .post('/')
+      .set('Accept', 'text/plain')
+      .expect('Content-Type', /text\/plain/)
       .expect('Strict-Transport-Security', /max-age/)
       .expect(403, done)
     })
